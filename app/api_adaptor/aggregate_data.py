354f556908cf8e505dcbb25e6b58bcf9a202a3e4
Charles Huang <fbcharles737@gmail.com>
2024-10-18 11:15:19 -0600

    annotate the weather data

--- app/api_adaptor/aggregate_data.py
+++ app/api_adaptor/aggregate_data.py
@@ -1,29 +1,31 @@
 from pydantic import BaseModel,Field
 from typing import Annotated
 from datetime import datetime
+from fastapi import Body
 
 class Weather_Data(BaseModel):
     # General weather data
-    temp: float | None
-    feels_like: float | None = None
-    temp_min: float | None = None
-    temp_max: float | None = None
-    pressure: float | None = None
-    humidity: float | None = None
-    sea_level: float | None = None
-    grnd_level: float | None = None
+    temp: Annotated[float, Body(description="Temperature in Celsius (°C)")]
+    feels_like: Annotated[float | None, Body(description="Feels like temperature in Celsius (°C)")] = None
+    temp_min: Annotated[float | None, Body(description="Minimum temperature in Celsius (°C)")] = None
+    temp_max: Annotated[float | None, Body(description="Maximum temperature in Celsius (°C)")] = None
+    pressure: Annotated[float | None, Body(description="Atmospheric pressure in hPa")] = None
+    humidity: Annotated[float | None, Body(description="Humidity percentage (%)")] = None
+    sea_level: Annotated[float | None, Body(description="Sea level pressure in hPa")] = None
+    grnd_level: Annotated[float | None, Body(description="Ground level pressure in hPa")] = None
 
     # Wind data
-    wind_speed: float | None
-    wind_deg: float | None
+    wind_speed: Annotated[float , Body(description="Wind speed in meters per second (m/s)")]
+    wind_deg: Annotated[float | None, Body(description="Wind direction in degrees (°)")] = None
 
     # Solar Irradiance Data
-    ghi:float
-    dni:float
-    dhi:float
-    irradiance_timestamp:datetime
+    ghi: Annotated[float, Body(description="Global horizontal irradiance in W/m²")]
+    dni: Annotated[float, Body(description="Direct normal irradiance in W/m²")]
+    dhi: Annotated[float, Body(description="Diffuse horizontal irradiance in W/m²")]
+    irradiance_timestamp: Annotated[datetime, Body(description="UTC timestamp of the irradiance data")]
 
     def __init__(self,**kwarg):
         super().__init__(**kwarg)
+
 
 
