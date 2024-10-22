93251327adf758cf1a400b4e9371ef7158c1bc5c
Charles Huang <fbcharles737@gmail.com>
2024-10-21 21:26:17 -0600

    simplify the device model
    Keep the most common parameters of inverters and solar module

--- app/models/solar_module.py
+++ app/models/solar_module.py
@@ -3,28 +3,19 @@ from typing import Annotated
 from fastapi import Body
 
 class SolarModuleModel(BaseModel):
-    Name: Annotated[str, Body(description="Name of the solar module")]="Name doesn't matter"
-    BIPV: Annotated[str, Body(description="Bifacial module indicator (Y/N)")]="N"
-    Date: Annotated[str, Body(description="Manufacture date of the module")]="Date doesn't matter"
-    T_NOCT: Annotated[float, Body(description="Module NOCT temperature rating (°C)")]
-    A_c: Annotated[float, Body(description="Area of singular solar panel (m²)")]
-    N_s: Annotated[int, Body(description="Number of cells in series")]
-    I_sc_ref: Annotated[float, Body(description="Short circuit current (A) at STC")]
-    V_oc_ref: Annotated[float, Body(description="Open circuit voltage (V) at STC")]
-    I_mp_ref: Annotated[float, Body(description="Max Power Current (A) at STC")]
-    V_mp_ref: Annotated[float, Body(description="Max power voltage (V) at STC")]
-    alpha_sc: Annotated[float, Body(description="Short circuit current change per °C (V/°C)")]
-    beta_oc: Annotated[float, Body(description="Open circuit voltage change per °C (V/°C)")]
-    a_ref: Annotated[float, Body(description="Ideality factor (V) from CEC module database")]
-    I_L_ref: Annotated[float, Body(description="Reference light current (A)")]
-    I_o_ref: Annotated[float, Body(description="Reference diode saturation current (A)")]
-    R_s: Annotated[float, Body(description="Reference series resistance (Ω)")]
-    R_sh_ref: Annotated[float, Body(description="Reference shunt resistance (Ω)")]
-    Adjust: Annotated[float, Body(description="Temperature coefficient adjustment factor")]
-    gamma_r: Annotated[float, Body(description="Gamma (%/°C)")]
-    Version: Annotated[str, Body(description="Version of the module")]="No version"
-    PTC: Annotated[float, Body(description="PTC on CEC datasheet")]
-    Technology: Annotated[str, Body(description="Technology type of the solar module")]="technology doesn't matter"
+
+    Name: Annotated[str, Body(description="Name will be specified by the system")]="Name will be specified by the system"
+    I_sc_ref: Annotated[float, Body(description="User-specified Short circuit current (A) at STC")]
+    diff_I_sc_ref: Annotated[float, Body(description="Difference from user-specified short circuit current (A)")]=0
+    
+    V_oc_ref: Annotated[float, Body(description="user-specified Open circuit voltage (V) at STC")]
+    diff_V_oc_ref: Annotated[float, Body(description="Difference from user-specified open circuit voltage (V)")]=0
+    
+    I_mp_ref: Annotated[float, Body(description="User-specified Max Power Current (A) at STC")]
+    diff_I_mp_ref: Annotated[float, Body(description="Difference from user-specified max power current (A)")]=0
+    
+    V_mp_ref: Annotated[float, Body(description="User-specified Max power voltage (V) at STC")]
+    diff_V_mp_ref: Annotated[float, Body(description="Difference from user-specified max power voltage (V)")]=0
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
