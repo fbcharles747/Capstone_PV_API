488386b0f5558e8b63a201986ee6f3e54345bd7d
Charles Huang <fbcharles737@gmail.com>
2024-10-23 22:55:53 -0600

    re-ordering device model so it work with pvlib

--- app/models/inverter.py
+++ app/models/inverter.py
@@ -3,24 +3,22 @@ from typing import Annotated
 from fastapi import Body
 
 class InverterModel(BaseModel):
-    Name: Annotated[str, Body(description="Name of the inverter will be specified for you by the system")]="No Name"
+    
     Vac: Annotated[float, Body(description="Nominal AC voltage (V)")]
-    Paco: Annotated[float, Body(description="Maximum AC power (W)")]
-    Vdco: Annotated[float, Body(description="Nominal DC voltage (V)")]
     Pso: Annotated[float, Body(description="Power consumption during operation (W)")]
+    Paco: Annotated[float, Body(description="Maximum AC power (W)")]
+    Pdco: Annotated[float, Body(description="Maximum DC power (W)")]
+    Vdco: Annotated[float, Body(description="Nominal DC voltage (V)")]
+    C0: Annotated[float, Body(description="Curvature between AC power and DC power (1/W)")]
+    C1: Annotated[float, Body(description="Coefficient of `Pdco` variation with DC input voltage (1/V)")]
+    C2: Annotated[float, Body(description="Coefficient of inverter power consumption loss variation with DC input voltage (1/V)")]
+    C3: Annotated[float, Body(description="Coefficient of C0 variation with DC input voltage (1/V)")]
+    Pnt: Annotated[float, Body(description="Inverter night time loss (kW)")]
     Vdcmax: Annotated[float, Body(description="Maximum DC voltage (V)")]
     Idcmax: Annotated[float, Body(description="Maximum DC current (A)")]
     Mppt_low: Annotated[float, Body(description="Minimum MPPT DC voltage (V)")]
     Mppt_high: Annotated[float, Body(description="Maximum MPPT DC voltage (V)")]
-
-    diff_Vac: Annotated[float, Body(description="Difference from user-specified Vac")] = 0
-    diff_Paco: Annotated[float, Body(description="Difference from user-specified Paco")] = 0
-    diff_Vdco: Annotated[float, Body(description="Difference from user-specified Vdco")] = 0
-    diff_Pso: Annotated[float, Body(description="Difference from user-specified Pso")] = 0
-    diff_Vdcmax: Annotated[float, Body(description="Difference from user-specified Vdcmax")] = 0
-    diff_Idcmax: Annotated[float, Body(description="Difference from user-specified Idcmax")] = 0
-    diff_Mppt_low: Annotated[float, Body(description="Difference from user-specified Mppt_low")] = 0
-    diff_Mppt_high: Annotated[float, Body(description="Difference from user-specified Mppt_high")] = 0
+    Name: Annotated[str, Body(description="Name of the inverter")]="No Name"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
--- app/models/solar_module.py
+++ app/models/solar_module.py
@@ -4,18 +4,32 @@ from fastapi import Body
 
 class SolarModuleModel(BaseModel):
 
-    Name: Annotated[str, Body(description="Name will be specified by the system")]="Name will be specified by the system"
-    I_sc_ref: Annotated[float, Body(description="User-specified Short circuit current (A) at STC")]
-    diff_I_sc_ref: Annotated[float, Body(description="Difference from user-specified short circuit current (A)")]=0
+    Technology: Annotated[str, Body(description="Technology type of the solar module")]="Mono-c-Si"
+    Bifacial: Annotated[int, Body(description="Bifacial indicator (1 for Yes, 0 for No)")] = 1  # New field
+    STC: Annotated[float, Body(description="Standard Test Conditions (STC) related values")] = 550.62  # New field
+    PTC: Annotated[float, Body(description="Performance Test Conditions (PTC) related values")] = 516.4  # New field
+    A_c: Annotated[float, Body(description="Area of singular solar panel (m²)")] = 2.52
+    Length: Annotated[float, Body(description="Length of the solar module (m)")] = None  # NaN is represented as None in Python
+    Width: Annotated[float, Body(description="Width of the solar module (m)")] = None   # NaN is represented as None in Python
+    N_s: Annotated[int, Body(description="Number of cells in series")] = 72
+    I_sc_ref: Annotated[float, Body(description="Short circuit current (A) at STC")] = 14
+    V_oc_ref: Annotated[float, Body(description="Open circuit voltage (V) at STC")] = 49.9
+    I_mp_ref: Annotated[float, Body(description="Max Power Current (A) at STC")] = 13.11
+    V_mp_ref: Annotated[float, Body(description="Max power voltage (V) at STC")] = 42
+    alpha_sc: Annotated[float, Body(description="Short circuit current change per °C (V/°C)")] = 0.00546
+    beta_oc: Annotated[float, Body(description="Open circuit voltage change per °C (V/°C)")] = -0.13024
+    T_NOCT: Annotated[float, Body(description="Module NOCT temperature rating (°C)")] = 44.8
+    a_ref: Annotated[float, Body(description="Ideality factor (V) from CEC module database")] = 1.82357
+    I_L_ref: Annotated[float, Body(description="Reference light current (A)")] = 14.0167
+    I_o_ref: Annotated[float, Body(description="Reference diode saturation current (A)")] = 1.78e-11
+    R_s: Annotated[float, Body(description="Reference series resistance (Ω)")] = 0.164846
+    R_sh_ref: Annotated[float, Body(description="Reference shunt resistance (Ω)")] = 138.077
+    Adjust: Annotated[float, Body(description="Temperature coefficient adjustment factor")] = 6.23131
+    gamma_r: Annotated[float, Body(description="Gamma (%/°C)")] = -0.33
+    BIPV: Annotated[str, Body(description="Bifacial module indicator (Y/N)")] = "N"
+    Version: Annotated[str, Body(description="Version of the module")] = "2023.10.31"
+    Date: Annotated[str, Body(description="Manufacture date of the module")] = "11/16/2022"
+    Name: Annotated[str, Body(description="Name of the solar module")] = "JA Solar JAM72D30-550/MB"  # New field
     
-    V_oc_ref: Annotated[float, Body(description="user-specified Open circuit voltage (V) at STC")]
-    diff_V_oc_ref: Annotated[float, Body(description="Difference from user-specified open circuit voltage (V)")]=0
-    
-    I_mp_ref: Annotated[float, Body(description="User-specified Max Power Current (A) at STC")]
-    diff_I_mp_ref: Annotated[float, Body(description="Difference from user-specified max power current (A)")]=0
-    
-    V_mp_ref: Annotated[float, Body(description="User-specified Max power voltage (V) at STC")]
-    diff_V_mp_ref: Annotated[float, Body(description="Difference from user-specified max power voltage (V)")]=0
-
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
