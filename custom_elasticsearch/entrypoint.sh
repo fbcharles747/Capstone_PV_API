596605d9236bfdac2a3323f54f487833703e61ab
Charles Huang <fbcharles737@gmail.com>
2024-11-20 22:52:51 -0700

    update

--- custom_elasticsearch/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /run/secrets/elasticsearch_password /usr/share/elasticsearch/config/elasticsearch_password
-chmod 0400 /usr/share/elasticsearch/config/elasticsearch_password
-export ELASTIC_PASSWORD_FILE=/usr/share/elasticsearch/config/elasticsearch_password
-exec /usr/local/bin/docker-entrypoint.sh
