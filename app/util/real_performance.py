12666437d8f2dfc938d55f321dd2298554610880
Charles Huang <fbcharles737@gmail.com>
2025-03-09 14:53:59 -0600

    Update real_performance.py

--- app/util/real_performance.py
+++ app/util/real_performance.py
@@ -4,7 +4,7 @@ from math import pow
 
 def estimate_real_performance(result:ModelResult,weather:Weather_Data)->ModelResult:
     ac_regression:float=(-0.0123379785) * weather.ghi + (0.000545750613) * weather.dni + (0.006399367634) * weather.dhi + (0.000001157367694)* result.system_ac_power
-    result.system_ac_power=ac_regression * pow(10,6)
+    result.system_ac_power=ac_regression * pow(10,6) if ac_regression >= 0 else 0
 
     return result
 
