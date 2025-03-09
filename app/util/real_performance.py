bc64d271d878817cdd2af0d861bbad4f1caa63ab
Charles Huang <fbcharles737@gmail.com>
2025-03-09 14:52:17 -0600

    update real performance formula

--- app/util/real_performance.py
+++ app/util/real_performance.py
@@ -3,7 +3,7 @@ from app.api_adaptor.aggregate_data import Weather_Data
 from math import pow
 
 def estimate_real_performance(result:ModelResult,weather:Weather_Data)->ModelResult:
-    ac_regression:float=(-0.0722104769) * weather.ghi + (-0.00692898659) * weather.dni + (0.0120536989) * weather.dhi + (0.0000023656745)* result.system_ac_power
+    ac_regression:float=(-0.0123379785) * weather.ghi + (0.000545750613) * weather.dni + (0.006399367634) * weather.dhi + (0.000001157367694)* result.system_ac_power
     result.system_ac_power=ac_regression * pow(10,6)
 
     return result
